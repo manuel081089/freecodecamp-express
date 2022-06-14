5d04f658c846c305ad06da790a70992014835b6c
manuel081089 <manuel081089@gmail.com>
2022-06-14 13:50:50 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,17 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname +'/views/index.html');
+});
+
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"});
+});
+
+
 
 
 
